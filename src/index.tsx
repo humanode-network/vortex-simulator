04746b6a2606af8f21dc064f2d290a75b6ab1311
MOZGIII <mike-n@narod.ru>
2025-11-16 19:08:53 +0400

    Add react

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+const rootEl = document.getElementById("root");
+if (rootEl === null) {
+  throw new Error("no root");
+}
+
+const root = ReactDOM.createRoot(rootEl);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
