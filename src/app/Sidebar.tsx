d9416d1e2b687930bb4367f341378220fdcb5190
MOZGIII <mike-n@narod.ru>
2025-11-18 02:11:44 +0400

    Be more explicit about the router demo and avoid paths collision

--- src/app/Sidebar.tsx
+++ src/app/Sidebar.tsx
@@ -6,6 +6,9 @@ const AppShell: React.FC<React.PropsWithChildren> = ({ children }) => {
         <span className="sidebar__logo" aria-hidden="true"></span>
       </div>
       <nav className="sidebar__nav" aria-label="Primary">
+        <a className="sidebar__link" data-nav-link="human-nodes" href="/page1">
+          Page 1 (route demo)
+        </a>
         <a
           className="sidebar__link"
           data-nav-link="human-nodes"
