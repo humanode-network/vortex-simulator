ed91cf64a23119d69a9d785f503f4ba49004b0c5
MOZGIII <mike-n@narod.ru>
2025-11-18 10:07:25 +0400

    Add mui

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const rootEl = document.getElementById("root");
 if (rootEl === null) {
   throw new Error("no root");
