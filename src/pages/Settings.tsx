3cbe102406d44db5a8bba507c6d9232bfbb61ba2
Cyber-preacher <davidkavazi@gmail.com>
2025-11-18 14:24:48 +0400

    Routes updated, react import wired, html archived

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,10 @@
+const Settings: React.FC = () => {
+  return (
+    <div className="app-page">
+      <h1>Settings</h1>
+      <p>Manage preferences and account settings here.</p>
+    </div>
+  );
+};
+
+export default Settings;
---
+++ src/pages/factions/Factions.tsx
@@ -0,0 +1,10 @@
+const Factions: React.FC = () => {
+  return (
+    <div className="app-page">
+      <h1>Factions</h1>
+      <p>Faction overview will go here.</p>
+    </div>
+  );
+};
+
+export default Factions;
