a1c2a6d8c9d5ee716e5480359fa740c13ff57794
MOZGIII <mike-n@narod.ru>
2025-11-18 15:02:58 +0400

    Drop mui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
 import AppRoutes from "./AppRoutes";
 import { BrowserRouter } from "react-router";
-import CssBaseline from "@mui/material/CssBaseline";
 
 const App: React.FC = () => {
   return (
     <>
-      <CssBaseline />
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
